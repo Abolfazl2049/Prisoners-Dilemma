e6a838a94a2451657abf49e5607bd4fd8cfe0193
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2024-04-19 23:30:21 +0330

    start && Oppening-page

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<script setup>
+import {ref, onMounted} from "vue";
+let uiStateManager = ref({
+  oppeningImg: false
+});
+let videoOppening = ref();
+onMounted(() => {
+  setInterval(() => {
+    uiStateManager.value.oppeningImg = false;
+    videoOppening.value.classList.remove("hidden");
+    videoOppening.value.play();
+    setTimeout(() => {
+      uiStateManager.value.oppeningImg = true;
+      videoOppening.value.classList.add("hidden");
+    }, 1250);
+  }, 7000);
+  setTimeout(() => {
+    uiStateManager.value.oppeningImg = true;
+    videoOppening.value.classList.add("hidden");
+  }, 1350);
+});
+</script>
+<template>
+  <div class="w-[100%] h-[100vh] relative">
+    <img
+      src="/imgs/openning.jpg"
+      style="object-fit: fill"
+      class="h-[100%] w-[100%] absolute z-[0]"
+      alt=""
+      v-if="uiStateManager.oppeningImg"
+    />
+    <div class="absolute bg-[rgba(255,255,255,0.10)] inset-0 ">
+      <div
+        class="*:m-2 *:w-[100%] *:h-[75px] *:border-[4px] *:transition-all *:border-white *:cursor-pointer *:text-[20px] *:text-center *:pt-[22px] flex flex-col mx-auto w-[clamp(250px,50%,600px)] mt-[30vh] items-center"
+      >
+        <div class="hover:bg-white text-white hover:text-[black]">
+          Leaderboard
+        </div>
+        <div class="bg-white hover">Play</div>
+        <div class="hover:bg-white text-white hover:text-[black]">Profile</div>
+      </div>
+    </div>
+    <video
+      autoplay
+      muted
+      src="/videos/Openning-video.mp4"
+      ref="videoOppening"
+      style="object-fit: fill"
+      class="h-[100%] w-[100%]"
+    ></video>
+  </div>
+</template>
