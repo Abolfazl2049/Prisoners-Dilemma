00d3d396b3483b6466bb4ada193237f7c52a8041
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2024-04-21 10:40:22 +0330

    changes && improvements

--- pages/index.vue
+++ pages/index.vue
@@ -12,38 +12,43 @@ onMounted(() => {
     setTimeout(() => {
       uiStateManager.value.oppeningImg = true;
       videoOppening.value.classList.add("hidden");
-    }, 1250);
+    }, 1650);
   }, 7000);
   setTimeout(() => {
     uiStateManager.value.oppeningImg = true;
     videoOppening.value.classList.add("hidden");
-  }, 1350);
+  }, 1650);
 });
 </script>
 <template>
   <div class="w-[100%] h-[100vh] relative">
     <img
-      src="/imgs/openning.jpg"
+      src="/imgs/questionMark.png"
+      class="hover:scale-[1.1] transition-all cursor-pointer absolute w-[60px] z-[20]"
+      alt=""
+    />
+    <img
+      src="/imgs/Oppening-img2.jpg"
       style="object-fit: fill"
       class="h-[100%] w-[100%] absolute z-[0]"
       alt=""
       v-if="uiStateManager.oppeningImg"
     />
-    <div class="absolute bg-[rgba(255,255,255,0.10)] inset-0 ">
+    <div class="absolute bg-[rgba(255,255,255,0.10)] inset-0">
       <div
         class="*:m-2 *:w-[100%] *:h-[75px] *:border-[4px] *:transition-all *:border-white *:cursor-pointer *:text-[20px] *:text-center *:pt-[22px] flex flex-col mx-auto w-[clamp(250px,50%,600px)] mt-[30vh] items-center"
       >
         <div class="hover:bg-white text-white hover:text-[black]">
           Leaderboard
         </div>
-        <div class="bg-white hover">Play</div>
+        <div class="hover:bg-white hover:text-black">Play</div>
         <div class="hover:bg-white text-white hover:text-[black]">Profile</div>
       </div>
     </div>
     <video
       autoplay
       muted
-      src="/videos/Openning-video.mp4"
+      src="/videos/Oppening-video2.mp4"
       ref="videoOppening"
       style="object-fit: fill"
       class="h-[100%] w-[100%]"
